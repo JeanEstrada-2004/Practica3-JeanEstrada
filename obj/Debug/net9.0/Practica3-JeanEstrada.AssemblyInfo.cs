941cf5586d9cc9bf903d202f5f9e41a804de71a0
JeanEstrada-2004 <jeanestrada350@gmail.com>
2025-05-23 23:40:17 -0500

    Implementacion de  NewsController con vistas Index y Details enriquecidas

--- obj/Debug/net9.0/Practica3-JeanEstrada.AssemblyInfo.cs
+++ obj/Debug/net9.0/Practica3-JeanEstrada.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica3-JeanEstrada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e72666c541ce8cbd8c8172dfad01af90ece2416")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35e5316be2165ea7381a5ebce3d47298b96096d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica3-JeanEstrada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica3-JeanEstrada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
