7cdea3e966ebd24f086064a4d7896f992e715fbc
JeanEstrada-2004 <jeanestrada350@gmail.com>
2025-05-23 19:42:35 -0500

    Subiendo Practica 3 a GitHub

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practica3_JeanEstrada.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
